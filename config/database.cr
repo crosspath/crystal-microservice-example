7ad073e2d87051b71ab990d47bda927294b79083
Evgeniy Nochevnov <xpath@e1.ru>
2019-02-06 14:46:49 +0300

    empty project

---
+++ config/database.cr
@@ -0,0 +1,20 @@
+database = "bonusapp_#{Lucky::Env.name}"
+
+LuckyRecord::Repo.configure do |settings|
+  if Lucky::Env.production?
+    settings.url = ENV.fetch("DATABASE_URL")
+  else
+    settings.url = ENV["DATABASE_URL"]? || LuckyRecord::PostgresURL.build(
+      hostname: "localhost",
+      database: database
+
+      # Optional settings:
+      #
+      # username: "postgres",
+      # password: "postgres",
+      # port: 5432
+    )
+  end
+  # In development and test, raise an error if you forget to preload associations
+  settings.lazy_load_enabled = Lucky::Env.production?
+end
